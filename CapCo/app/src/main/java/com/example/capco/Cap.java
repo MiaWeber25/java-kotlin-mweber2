3cacdaf63478d4c5ecbf72dd070032befff3d642
Mia Weber <miaweber02@gmail.com>
2023-02-09 13:54:28 -0700

    json progress

---
+++ CapCo/app/src/main/java/com/example/capco/Cap.java
@@ -0,0 +1,37 @@
+package com.example.javabasics;
+
+public class Cap {
+   // private Size size = Size.MEDIUM; // If you don't initialize an object to something it is a null pointer. Try to do something with the object = null pointer exception
+    public static final Size DEFAULT_SIZE = Size.MEDIUM; // final is CONST in Java
+
+    private static Size size = DEFAULT_SIZE;
+
+
+    //Getters and Setters
+    public Size getSize() {
+        return size;
+    }
+
+    public void setSize(Size size) {
+        if (size == null) {
+            throw new IllegalArgumentException("size cannot be null");
+        }
+        this.size = size;
+    }
+
+    private String label = "";
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        if (label == null) {
+            throw new IllegalArgumentException("label cannot be null");
+        }
+        if (Utils.codepoints(label) > 20) { // Set a max character length for the label of 20
+            throw new IllegalArgumentException("label is to long");
+        }
+        this.label = label;
+    }
+}
