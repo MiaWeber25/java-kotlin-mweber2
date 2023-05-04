564716295d992e5d1a966703e94ae658c0bec2e8
Mia Weber <miaweber02@gmail.com>
2023-05-04 13:37:36 -0600

    currencyConverter tests done

---
+++ EspressoKotlin/app/src/androidTest/java/com/example/espressokotlin/ui/main/MainFragmentTest.kt
@@ -0,0 +1,16 @@
+package com.example.espressokotlin.ui.main
+
+import org.junit.Assert.*
+
+import org.junit.Test
+
+class MainFragmentTest {
+
+    @Test
+    fun onCreate() {
+    }
+
+    @Test
+    fun onCreateView() {
+    }
+}
---
+++ currencyConverter/app/src/main/java/com/example/currencyconverter/Util.java
@@ -0,0 +1,7 @@
+package com.example.currencyconverter;
+
+public class Calculate {
+    double calculate(double amount, double exchangeRate) {
+        return amount * exchangeRate;
+    }
+}
