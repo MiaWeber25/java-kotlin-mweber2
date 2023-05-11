c81f9aecdb28702f6da46bf2ac63c7c7147fc495
Mia Weber <miaweber02@gmail.com>
2023-05-11 14:34:32 -0600

    currencyConverter Kotlin App

---
+++ currencyConverterKotlin/app/src/main/java/com/example/currencyconverter/Util.kt
@@ -0,0 +1,8 @@
+package com.example.currencyconverter
+
+//object Util {
+   // @JvmStatic
+    fun calculate(amount: Double, exchangeRate: Double): Double {
+        return amount * exchangeRate
+    }
+//}
