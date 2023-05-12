a0873e26413031a9c7adee65b34117ab6dc60204
Mia Weber <miaweber02@gmail.com>
2023-05-11 20:48:25 -0600

    deleted currencyConverterKotlin

--- currencyConverterKotlin/app/src/main/java/com/example/currencyconverter/Util.kt
+++
@@ -1,8 +0,0 @@
-package com.example.currencyconverter
-
-//object Util {
-   // @JvmStatic
-    fun calculate(amount: Double, exchangeRate: Double): Double {
-        return amount * exchangeRate
-    }
-//}
--- currencyConverterKotlin/app/src/test/java/com/example/currencyconverterkotlin/MainActivityTest.kt
+++
@@ -1,5 +0,0 @@
-package com.example.currencyconverterkotlin
-
-import org.junit.Assert.*
-
-class MainActivityTest
